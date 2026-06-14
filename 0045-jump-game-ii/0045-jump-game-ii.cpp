0f88757a983651dd75c6c08b274448de8ff9fb0a
anonymous2264 <nilava_ug_23@ece.nits.ac.in>
2026-06-14 21:17:30 +0530

    Medium

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int l=0;
+        int r=0;
+        int j=0;
+        while(r<n-1)
+        {
+            int far=0;
+            for(int i=l;i<=r;i++)
+            {
+                far=max(far,i+nums[i]);
+            }
+            l=r+1;
+            j++;
+            r=far;
+        }
+        return j;
+    }
+};
